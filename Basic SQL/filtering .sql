f5862446ba6c69f500e6c7b8d26f497cb02d3e63
Gmusebe <musebeivan@gmail.com>
2022-06-11 18:03:33 +0300

    where statements

--- Basic SQL/filtering .sql
+++ Basic SQL/filtering .sql
@@ -8,3 +8,44 @@ FROM
   Cities
 WHERE
   area > 4000;
+
+-- Not equal to
+SELECT
+  name,
+  area
+FROM
+  Cities
+WHERE
+  area != 8223;
+-- Also <>
+
+-- Values falling between:
+SELECT
+  name,
+  area
+FROM
+  Cities
+WHERE
+  area BETWEEN 2000
+  AND 4000;
+
+--  in and not in
+SELECT
+  name,
+  area
+FROM
+  Cities
+WHERE
+  name IN ('Delhi', 'Shanghai');
+-- replace IN with NOT IN
+
+-- Compound conditions
+SELECT
+  name,
+  area
+FROM
+  Cities
+WHERE
+  area NOT IN (3043, 8223)
+  AND name = 'Delhi';
+-- replace AND with OR
