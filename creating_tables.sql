1171a8373c0f26dd86a94842ed43d0b6994ff868
Gmusebe <musebeivan@gmail.com>
2022-06-11 10:30:21 +0300

    insert values

--- creating_tables.sql
+++ creating_tables.sql
@@ -1,6 +1,14 @@
+-- Create Table
 CREATE TABLE Cities(
   name VARCHAR(50),
   country VARCHAR(50),
   population INTEGER,
   area INTEGER
 );
+
+-- Insert values in Table
+INSERT INTO Cities (name, country, population, area)
+VALUES
+	('Delhi', 'India', 28125000, 2240),
+  ('Shanghai', 'China', 22125000, 4015),
+  ('Sao Paulo', 'Brazil', 20935000, 3043);
