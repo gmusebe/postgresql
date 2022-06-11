05c38abca8c3395aafde454293cc77511dbe4032
Gmusebe <musebeivan@gmail.com>
2022-06-11 10:36:20 +0300

    retrieve data

--- creating_tables.sql
+++ creating_tables.sql
@@ -12,3 +12,10 @@ VALUES
 	('Delhi', 'India', 28125000, 2240),
   ('Shanghai', 'China', 22125000, 4015),
   ('Sao Paulo', 'Brazil', 20935000, 3043);
+
+-- Retrieve info from table
+-- * Retrieve all column data
+SELECT * FROM Cities;
+
+-- Retrieve specific column data
+SELECT name, country FROM Cities;
