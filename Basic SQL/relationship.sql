19f62f4b3068dfc205d9509f80570a78ec94a026
Gmusebe <musebeivan@gmail.com>
2022-10-17 15:47:53 +0300

    foreign key established

--- Basic SQL/relationship.sql
+++ Basic SQL/relationship.sql
@@ -15,3 +15,20 @@ VALUES
   ('pferrer'),
   ('si93onis'),
   ('99stroman');
+
+-- Second table with relatable foreign keys:
+CREATE TABLE photos(
+  id SERIAL PRIMARY KEY,
+  url VARCHAR(200),
+  -- Relationship generation:
+  user_id INTEGER REFERENCES users(id)
+);
+
+--  insert values:
+INSERT INTO
+  photos (url, user_id)
+VALUES
+  ('http://one.jpg', 4),
+  ('http://two.jpg',4),
+  ('http://three.jpg',1),
+  ('http://four.jpg',2);
