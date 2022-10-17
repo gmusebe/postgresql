791647800687ca3cdd7aae878c04398e58f6692d
Gmusebe <musebeivan@gmail.com>
2022-10-17 15:48:58 +0300

    join function

--- Basic SQL/relationship.sql
+++ Basic SQL/relationship.sql
@@ -32,3 +32,19 @@ VALUES
   ('http://two.jpg',4),
   ('http://three.jpg',1),
   ('http://four.jpg',2);
+
+-- Retrieving data in a meaningful way:
+-- Find all photos posted by user no.4:
+SELECT
+  *
+FROM
+  photos
+WHERE
+  user_id = 4;
+
+-- Join Statement(Challenging function to grasp in SQL; needs more time)
+SELECT
+  url, username
+FROM
+  photos
+  JOIN users ON users.id = photos.user_id;
