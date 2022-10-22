83f6211720ac5a819d43c470f725ffb88b0b4861
Gmusebe <musebeivan@gmail.com>
2022-10-22 16:37:01 +0300

    Three Way Join

--- Basic SQL/joins.sql
+++ Basic SQL/joins.sql
@@ -39,3 +39,26 @@ FROM
   INNER JOIN comments ON comments.photo_id = photos.id
 WHERE
   comments.user_id = photos.user_id;
+
+
+--  Three way join
+SELECT
+  url,
+  contents,
+  username
+FROM
+  photos
+  INNER JOIN comments ON comments.photo_id = photos.id
+  INNER JOIN users ON users.id = comments.user_id
+  AND users.id = photos.user_id
+
+Exercise:
+SELECT
+  title,
+  rating,
+  name
+FROM
+  books
+  JOIN reviews ON reviews.book_id = books.id
+  JOIN authors ON authors.id = reviews.reviewer_id
+  AND authors.id = books.author_id;
