ba4ea11d12fb88074be8d38045226cd8dcf0b631
Gmusebe <musebeivan@gmail.com>
2022-10-25 21:14:57 +0300

    Aggregate functions

--- Basic SQL/aggregates.sql
+++ Basic SQL/aggregates.sql
@@ -6,3 +6,43 @@ FROM
 GROUP BY
   User_id;
 -- Used to find the unique value in a column/variable
+
+--  Aggregate function
+-- COUNT()
+-- How many ids do we have all together:
+SELECT
+  COUNT(id)
+FROM
+  comments;
+
+
+-- SUM()
+--  What is the sum of all the ids:
+SELECT
+  SUM(id)
+FROM
+  comments;
+
+-- AVG()
+-- Average id:
+SELECT
+  AVG(id)
+FROM
+  comments;
+
+
+-- MIN()
+-- Fid minimum id:
+SELECT
+  MIN(id)
+FROM
+  comments;
+
+
+-- MAX()
+-- Find comment with the largest id:
+SELECT
+  MAX(id)
+FROM
+  comments;
+
