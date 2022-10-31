5fcebc36f99709ae9e0340ad807d3dc172b2d80c
Gmusebe <musebeivan@gmail.com>
2022-10-31 22:33:15 +0300

    subqueries examples

--- Basic SQL/subquery.sql
+++ Basic SQL/subquery.sql
@@ -13,3 +13,36 @@ WHERE
     WHERE
     department = 'Toys'
   );
+
+
+-- Sub example:
+SELECT
+  name,
+  price,
+  (
+    SELECT
+      MAX(price)
+    FROM
+      products
+  )
+FROM
+  products
+WHERE
+  price > 876;
+
+-- Ex.2
+SELECT
+  name,
+  price,
+  (
+    SELECT
+      price
+    FROM
+      products
+    WHERE
+    	id = 3
+  ) AS id_3_price
+FROM
+  products
+WHERE
+  price > 876;
