c00124a159b27bf630f8644723e9bedfeb3db003
Musebe Ivan <musebeivan@gmail.com>
2022-11-06 15:44:01 +0300

    SELECT multiple subqueries:

--- Basic SQL/subquery.sql
+++ Basic SQL/subquery.sql
@@ -227,3 +227,24 @@ SELECT (
   SELECT MAX(price)
   FROM products
 );
+
+-- A select with three subqueries:
+SELECT
+  (
+    SELECT
+      MAX(price)
+    FROM
+      phones
+  ) AS max_price,
+  (
+    SELECT
+      MIN(price)
+    FROM
+      phones
+  ) AS min_price,
+  (
+    SELECT
+      AVG(price)
+    FROM
+      phones
+  ) AS avg_price;
