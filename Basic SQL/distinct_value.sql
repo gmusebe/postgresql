064ffe947bf8dcdd831835e0e34f8456fe618273
Musebe Ivan <musebeivan@gmail.com>
2022-11-07 10:15:24 +0300

    distinct values

---
+++ Basic SQL/distinct_value.sql
@@ -0,0 +1,18 @@
+-- The function will give you a list of all unique values inside of a column.
+-- USE; print out the number of unique values
+SELECT
+  DISTINCT department
+FROM
+  products;
+
+SELECT
+  COUNT(DISTINCT department)
+FROM
+  products;
+-- Similar to GROUP BY
+
+-- Multiple unique values unique combinations
+SELECT
+  DISTINCT department, name
+FROM
+  products;
