788871974800ca676401fd5b6cacb1954638aa98
Musebe Ivan <musebeivan@gmail.com>
2022-12-13 09:24:36 +0300

    default record values

--- postgrel-pgadmin/insert tables.sql
+++ postgrel-pgadmin/insert tables.sql
@@ -1,21 +1,35 @@
--- CREATE TABLE products(
--- 	id SERIAL PRIMARY KEY,
--- 	name VARCHAR(40),
--- 	department VARCHAR(40),
--- 	price INTEGER,
--- 	weight INTEGER
--- );
+CREATE TABLE products(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40),
+	department VARCHAR(40),
+	price INTEGER,
+	weight INTEGER
+);
 
--- INSERT INTO products(name, department, weight)
--- VALUES
--- 	('Pants', 'Clothes', 3)
+INSERT INTO products(name, department, weight)
+VALUES
+	('Gloves', 'Tools', 1);
 
 -- Pre-validtion for null:
--- UPDATE products
--- SET price = 9999
--- WHERE price is NULL;
+UPDATE products
+SET price = 9999
+WHERE price is NULL;
 
 -- Setting validation:
 ALTER TABLE products
 ALTER COLUMN price
 SET NOT NULL;
+
+-- Upfront values
+CREATE TABLE products(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) NOT NULL,
+	department VARCHAR(40) NOT NULL,
+	price INTEGER DEFAULT 9999,
+	weight INTEGER
+);
+
+-- Set default
+ALTER TABLE products
+ALTER COLUMN price
+SET DEFAULT 9999;
