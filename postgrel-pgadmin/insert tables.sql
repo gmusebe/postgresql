ba7001703420cbfcc08cfd49fb62d66d21c314f1
Musebe Ivan <musebeivan@gmail.com>
2022-12-13 08:15:47 +0300

    sql in admin

---
+++ postgrel-pgadmin/insert tables.sql
@@ -0,0 +1,11 @@
+-- CREATE TABLE products(
+-- 	id SERIAL PRIMARY KEY,
+-- 	name VARCHAR(40),
+-- 	department VARCHAR(40),
+-- 	price INTEGER,
+-- 	weight INTEGER
+-- );
+
+INSERT INTO products(name, department, price, weight)
+VALUES
+	('Shirts', 'Clothes', 20, 1)
