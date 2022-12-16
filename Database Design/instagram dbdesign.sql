c54d7a225bb738bf6af16e4543c2ac8a1ca41c9c
Musebe Ivan <musebeivan@gmail.com>
2022-12-16 15:28:09 +0300

    instagram database tables

--- Database Design/instagram dbdesign.sql
+++ Database Design/instagram dbdesign.sql
@@ -1,3 +1,4 @@
+-- users table
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
 	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
@@ -10,4 +11,88 @@ CREATE TABLE users (
 	password VARCHAR(50),
 	status VARCHAR(15),
 	CHECK (COALESCE(phone, email) IS NOT NULL)
+);
+
+-- posts table
+CREATE TABLE posts (
+	id SERIAL PRIMARY KEY,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	url VARCHAR(200) NOT NULL,
+	caption VARCHAR(240),
+	lat REAL CHECK(lat IS NULL OR (lat >=-90 AND lat <=90)),
+	lng REAL CHECK(lat IS NULL OR (lng >=-180 AND lng <=180)),
+	user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- comments table
+CREATE TABLE comments (
+	id SERIAL PRIMARY KEY,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	content VARCHAR(240) NOT NULL,
+	user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	post_id INTEGER NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
+);
+
+-- likes/reaction resource
+CREATE TABLE reactions (
+	id SERIAL PRIMARY KEY,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	comment_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+	post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+	CHECK(
+		COALESCE((post_id)::BOOLEAN::INTEGER, 0)
+		+
+		COALESCE((comment_id)::BOOLEAN::INTEGER, 0)
+		= 1
+	),
+	-- a user to have one unique like to any given post or comment
+	UNIQUE(user_id, post_id, comment_id)
+);
+
+-- photo tags table
+CREATE TABLE photo_tags (
+	id SERIAL PRIMARY KEY,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	post_id INTEGER NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
+	x INTEGER NOT NULL,
+	y INTEGER NOT NULL,
+	UNIQUE(user_id, post_id)
+);
+
+-- caption tags table
+CREATE TABLE caption_tags (
+	id SERIAL PRIMARY KEY,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	post_id INTEGER NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
+	UNIQUE(user_id, post_id)
+);
+
+-- hashtags table
+CREATE TABLE hashtags(
+	id SERIAL PRIMARY KEY,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	title VARCHAR(20) NOT NULL UNIQUE
+);
+
+-- hashtag posts table 
+CREATE TABLE hashtag_posts(
+	id SERIAL PRIMARY KEY,
+	hashtag_id INTEGER NOT NULL REFERENCES hashtags(id) ON DELETE CASCADE,
+	post_id INTEGER NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
+	UNIQUE(hashtag_id, post_id)
+);
+
+-- followers table
+CREATE TABLE followers (
+	id SERIAL PRIMARY KEY,
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	follower_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	UNIQUE(user_id, follower_id)
 );
