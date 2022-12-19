cd44cbaadacedab4fb559d2c72cb045e013fc2fb
Musebe Ivan <musebeivan@gmail.com>
2022-12-19 11:30:41 +0300

    explain & analyse

--- Database Design/efficient queries.sql
+++ Database Design/efficient queries.sql
@@ -1,6 +1,11 @@
 -- Index - prevents a  full scan of a query thus enhances the performance of postgreSQL.
 -- An Index is an external record that tells us exactly where a record is stored inside of a heap file.
 
+-- Automated indices in the db:
+SELECT relname, relkind
+FROM pg_class
+WHERE relkind = 'i';
+
 -- creating an index
 CREATE INDEX ON users(username);
 
@@ -16,3 +21,18 @@ DROP INDEX users_username_idx;
 EXPLAIN ANALYSE SELECT *
 FROM users
 WHERE username = 'Emil30';
+
+
+-- Show Postgrel plan of execution:
+EXPLAIN SELECT username, contents
+FROM
+  users
+  JOIN comments ON comments.user_id = users.id
+WHERE username = 'Alyson14';
+
+-- execution efficiency in time:
+EXPLAIN SELECT username, contents
+FROM
+  users
+  JOIN comments ON comments.user_id = users.id
+WHERE username = 'Alyson14';
