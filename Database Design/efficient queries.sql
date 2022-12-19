faac50f601133cec6b851cbc83ae4b6c2a0b1704
Musebe Ivan <musebeivan@gmail.com>
2022-12-19 09:15:31 +0300

    index perfomance

--- Database Design/efficient queries.sql
+++ Database Design/efficient queries.sql
@@ -3,3 +3,11 @@
 
 -- creating an index
 CREATE INDEX ON users(username);
+
+-- Drop an Index:
+DROP INDEX users_username_idx;
+
+-- Test Performance with Index = 0.057ms, without Index = 1.242ms:
+EXPLAIN ANALYSE SELECT *
+FROM users
+WHERE username = 'Emil30';
