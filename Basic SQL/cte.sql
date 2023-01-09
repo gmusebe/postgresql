1f919ecc9fc11337cfd87b7777f7c4f3b8e9ca50
Musebe Ivan <musebeivan@gmail.com>
2023-01-09 20:32:12 +0300

    view changes

--- Basic SQL/cte.sql
+++ Basic SQL/cte.sql
@@ -51,7 +51,7 @@ WITH tags AS (
 )
 
 Alternative create a View
-CREATE VIEW tags AS(
+CREATE OR REPLACE VIEW tags AS(
   SELECT id, created_at, user_id, post_id, 'photo_tag' AS type FROM photo_tags
   UNION ALL
   SELECT id, created_at, user_id, post_id, 'caption_tag' AS type FROM caption_tags
@@ -62,3 +62,6 @@ FROM users
   JOIN tags ON users.id = tags.user_id
 GROUP BY users.username
 ORDER BY count DESC;
+
+-- Drop a view
+DROP VIEW title;
